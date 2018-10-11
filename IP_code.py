6309aaec0c29f3b769c537f97886507d3b4ed209
OnHoliday <konrad.piastka@gmail.com>
2018-10-11 18:06:56 +0100

    Add files via upload
    Use it to perform data preprocessing (to have the same missing data)

---
+++ IP_code.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import pandas as pd
+import random 
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+
+y = dataset.iloc[:, 13].values
+X = dataset.iloc[:, :-1].values
+
+# Deleting randomly some data
+i=0
+j=0
+for column in dataset:
+    i+=1
+    j=0
+    print('Columna: %d' %i)
+    for row in dataset.iterrows():
+        j+=1
+        if i*j%random.randint(1000,2000)==0:
+            X[j,i] = ''
+df = pd.DataFrame(X)
+
+# Here we can perform some data preprocessing 
+
+ 
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
